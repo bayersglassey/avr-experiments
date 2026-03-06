3f7d6f9f291a28ee811574dad1f8700476d4caf8
Ben Ayers-Glassey <bayersglassey@gmail.com>
2026-03-06 14:11:07 -0800

    updated

--- blink/blink.c
+++ blink/blink.c
@@ -49,6 +49,18 @@ static void uart_put(char c) {
     UDR0 = c;
 }
 
+static char uart_get_noblock(void) {
+    if (bit_is_set(UCSR0A, RXC0)) return UDR0;
+    else return '\0';
+}
+
+static bool uart_put_noblock(char c) {
+    if (bit_is_set(UCSR0A, UDRE0)) {
+        UDR0 = c;
+        return true;
+    } else return false;
+}
+
 
 void led_init(void) {
     DDRB |= (1 << PB5);
@@ -62,10 +74,30 @@ static void led_off(void) {
     PORTB &= ~(1 << PB5);
 }
 
+int add(int x, int y) { return x + y; }
+uint8_t add8(uint8_t x, uint8_t y) { return x + y; }
+int add3(int x, int y, int z) { return x + y + z; }
+uint8_t add38(uint8_t x, uint8_t y, uint8_t z) { return x + y + z; }
+int incp(int *x) { return *x + 1; }
+void incpp(int *x) { *x += 1; }
+int addp(int *x, int *y) { return *x + *y; }
+void addpp(int *x, int *y, int *result) { *result = *x + *y; }
+int f(int *x, int *y, void fp(int *, int *)) { fp(x, y); return *x + *y; }
+int g(void fp(int *, int *)) { int x = 2, y = 3; fp(&x, &y); return x + y; }
+int h(void fp(int *, int *)) { static int x = 2, y = 3; fp(&x, &y); return x + y; }
+
 
 int main(void) {
     led_init();
     uart_init();
+
+    (void)add(2, 3);
+    (void)add8(2, 3);
+    (void)add3(2, 3, 4);
+    (void)add38(2, 3, 4);
+    int x = 3, y = 4, z;
+    (void)addp(&x, &y);
+    (void)addpp(&x, &y, &z);
 
     while (1) {
         led_on();
--- compile.sh
+++ compile.sh
@@ -6,6 +6,7 @@ avr-gcc \
     -Wall \
     -Wno-unused-function \
     -Os \
+    "$@" \
     -o "$PROJFILE.elf" "$PROJFILE.c"
 
 avr-objcopy -j .text -j .data -O ihex "$PROJFILE.elf" "$PROJFILE.hex"
